55cd8561b1c847a3823876940e6755b7a99dd368
Francisco Sedeño Barea <sedenobareaf@iesmartinrivero.org>
2022-04-25 10:45:39 +0200

    Tercer Commit, Añadida ClaseFrancisco2

---
+++ src/login_22/ClaseFrancisco2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_22;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClaseFrancisco2 {
+    public ClaseFrancisco2(){
+        int i=0;
+        do{
+            System.out.println("Hola");
+            System.out.println("Adios");
+        }while(i<1);
+    }
+}
