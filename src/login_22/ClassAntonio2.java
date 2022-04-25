c42b52319e11dccdd60cc8e2ad6576c05a451d45
Antonio Lara <larapinedaaj@iesmartinrivero.org>
2022-04-25 10:54:11 +0200

    Segundo Commit, Añadida ClassAntonio

---
+++ src/login_22/ClassAntonio2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login_22;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClassAntonio2 {
+    
+    String author ="Antonio....segunda parte";
+}
